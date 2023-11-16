dbbe1071f85433be69a3e60c996f1d9016a7f0cc
Adam Priestman <adampriestman@gmail.com>
2023-11-16 15:04:23 +0000

    styling page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # get "lists", to: "lists#index"
   resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create delete]
+    resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: [:destroy]
 end
